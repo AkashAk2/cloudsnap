b8362c36ca44132695ce5ead2197483f4d2a3afa
Ashiklal Memanaparambil Asokalal <amem0005@student.monash.edu>
2023-06-05 01:40:05 +1000

    FrontEnd initial commit

---
+++ FrontEnd/script.js
@@ -0,0 +1,33 @@
+let totalTags = {};
+function addFormTags(id){
+    if(!(id in totalTags)){
+        totalTags[id] = 0;
+    }
+    totalTags[id] += 1;
+    let formGroupDiv = document.createElement("div");
+    formGroupDiv.innerHTML = `<label for="">Tag ${totalTags[id]}</label>
+        <div class="tag-group">
+        <input
+            class="form-control tag-name"
+            type="text"
+            placeholder="Enter tag name"
+            name="tag${totalTags[id]}"
+        />
+        <input
+            class="form-control tag-value"
+            type="text"
+            placeholder="Tag value or count"
+            name="tag${totalTags[id]}count"
+      />`;
+    
+    const element = document.getElementById(id);
+    element.appendChild(formGroupDiv);
+}
+
+function onLoginClick(){
+    window.location.href= "./index.html";
+}
+
+function onLogOut(){
+    window.location.href= "./login.html";
+}
