9af462de6de8049ba94f08a94dbfaf85b2a8308d
Ashiklal Memanaparambil Asokalal <amem0005@student.monash.edu>
2023-06-06 03:04:35 +1000

    added js code to handle get image button click

--- FrontEndBasic/script.js
+++ FrontEndBasic/script.js
@@ -23,3 +23,52 @@ function addFormTags(id){
     const element = document.getElementById(id);
     element.appendChild(formGroupDiv);
 }
+
+// GET IMAGE Functionality
+document.addEventListener("DOMContentLoaded", function() {
+    // Assuming your "Get Image" button has an id of "getImageButton"
+    document.getElementById("getImageButton").addEventListener("click", function(event) {
+        event.preventDefault();  // prevent the default form submission
+
+        let formData = new URLSearchParams();
+
+        // Assuming your form has an id of "get-image-form"
+        let form = document.getElementById("get-image-form");
+
+        // Gather the form data
+        for(let i = 1; i <= totalTags["getImageByTags"]; i++) {
+            let tagName = form.querySelector(`input[name="tag${i}"]`).value;
+            let tagCount = form.querySelector(`input[name="tag${i}count"]`).value;
+
+            if(tagName && tagCount) {
+                formData.append(`tag${i}`, tagName);
+                formData.append(`tag${i}count`, tagCount);
+            }
+        }
+
+        // Create the full URL
+        let url = 'https://6irmyz4kvg.execute-api.us-east-1.amazonaws.com/test/search?' + formData.toString();
+
+        fetch(url, {
+            method: 'GET',
+        })
+            .then(response => response.json())
+            .then(data => {
+                // Switch to the "Results" tab
+                document.getElementById("ex1-tab-6").click();
+
+                // Clear the previous results
+                document.getElementById("resultsContainer").innerHTML = '';
+
+                // Append the new results
+                data.forEach(item => {
+                    let resultDiv = document.createElement("div");
+                    resultDiv.textContent = JSON.stringify(item);
+                    document.getElementById("resultsContainer").appendChild(resultDiv);
+                });
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    });
+});
